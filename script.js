71858f2bc8473a75747d6f1a4b9ea95a259bca8d
bando9 <bandomega1@gmail.com>
2023-01-02 22:34:55 +0700

    semua fitur telah selesai

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+// membuat sticky header
 const header = document.querySelector("header")
 
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 0)
 })
+// selesai membuat sticky header
+
+
+// membuat menu burger
+let menu = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+// selesai menu burger
+
+
+const sr = ScrollReveal ({
+    distance: '25px',
+    duration: 2500,
+    reset: true
+})
+
+sr.reveal('.home-text', {delay:190, origin: 'bottom'})
+
+sr.reveal('.about,.services,.portfolio,.contact', {delay:200, origin: 'bottom'})
